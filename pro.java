7bd6ba58397a211b307fdb191f0d04a2211765a8
Shivaraja395 <Shivaraja395>
2024-10-28 16:46:45 +0530

    dd

---
+++ pro.java
@@ -0,0 +1,12 @@
+class Builder 
+{
+	public static void main(String[] args) 
+	{
+		StringBuilder s1;
+		s1 =new StringBuilder();
+		System.out.println(s1.capacity());
+		s1.append("rama");
+		System.out.println(s1);
+	}
+}
+
